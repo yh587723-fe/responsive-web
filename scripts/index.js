0b6b1c05fdc2ab02cd21c62f24f14889bd6a2366
hyeseop <yh587723@gmail.com>
2026-05-15 10:10:02 +0900

    js 80% 진행중

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".popup-zone .swiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
