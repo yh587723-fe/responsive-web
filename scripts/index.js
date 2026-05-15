74f819729e384db9cd3e385fe2ca6655cc27eac0
hyeseop <yh587723@gmail.com>
2026-05-15 11:07:08 +0900

    js 80% 진행중

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
-var swiper = new Swiper(".popup-zone .swiper", {
+var swiper = new Swiper(".popup-zone .swiper ", {
+      slidesPerView: 'auto', //cdn 슬라이더 너비 고정 초기화
       spaceBetween: 30,
+      loop: true,
       centeredSlides: true,
       autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".next-btn",
+        prevEl: ".prev-btn",
       },
+
     });
